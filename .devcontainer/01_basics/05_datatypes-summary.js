b1faecb943307d58ecdd9ce20fb3466e309bd19c
Yogiejay <70306871+Yogiejay@users.noreply.github.com>
2023-11-24 15:14:59 +0000

    call by value and reference

--- .devcontainer/01_basics/05_datatypes-summary.js
+++ .devcontainer/01_basics/05_datatypes-summary.js
@@ -18,3 +18,28 @@ const id = Symbol('123')
 const anotherid = Symbol('123')
 
 console.log(id, anotherid)
+
+// callbyvalue and callburefrence
+//CALLBYVALUE IT just copies the address of given varibale at initialization
+//callby refrence it refrences the actual refrence of a varibale generally array object function
+// let var1 = function addition(a, b) {
+//     return a + b;
+// };
+// let var2 = var1;
+// var1 = addition(2, 3)
+// console.log(var2())
+// var1 = addition(3, 4)
+// console.log(var2())
+
+function addition(a, b) {
+    return a + b;
+}
+
+let var1 = addition(2, 3);
+let var2 = var1;
+console.log(var2);
+var1 = addition(3, 4);
+console.log(var2); // Output: 5
+
+
+
--- .devcontainer/01_basics/dist/05_datatypes-summary.dev.js
+++ .devcontainer/01_basics/dist/05_datatypes-summary.dev.js
@@ -16,4 +16,24 @@ console.log(vars);
 console.log(varss);
 var id = Symbol('123');
 var anotherid = Symbol('123');
-console.log(id, anotherid);
+console.log(id, anotherid); // callbyvalue and callburefrence
+//CALLBYVALUE IT just copies the address of given varibale at initialization
+//callby refrence it refrences the actual refrence of a varibale generally array object function
+// let var1 = function addition(a, b) {
+//     return a + b;
+// };
+// let var2 = var1;
+// var1 = addition(2, 3)
+// console.log(var2())
+// var1 = addition(3, 4)
+// console.log(var2())
+
+function addition(a, b) {
+  return a + b;
+}
+
+var var1 = addition(2, 3);
+var var2 = var1;
+console.log(var2);
+var1 = addition(3, 4);
+console.log(var2); // Output: 5
