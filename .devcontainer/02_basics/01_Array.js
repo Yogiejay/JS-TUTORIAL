c167f3a1f74fd5f9daf04528367f627bc5d69045
Yogiejay <70306871+Yogiejay@users.noreply.github.com>
2023-12-25 17:15:01 +0000

    DomManipulation

---
+++ .devcontainer/02_basics/01_Array.js
@@ -0,0 +1,15 @@
+let array = [1, 2, 3, 4, 5]
+// console.log(array);
+// let NewArray = new Array("a", "b", 6, 7)
+// array.push(8)
+// console.log(array);
+// array.pop()
+// console.log(array);
+array.shift(9)
+console.log(array);
+array.unshift(9)
+console.log(array);
+console.log(array.slice(1, 3))
+console.log(array);
+console.log(array.splice(1, 3))
+console.log(array);
